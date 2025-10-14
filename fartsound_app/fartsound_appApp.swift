64849041a4e5662b05ae9179f843db569e139b3e
YongT <aetl1003@gmail.com>
2025-10-14 09:29:12 +0900

    Initial Commit

---
+++ fartsound_app/fartsound_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  fartsound_appApp.swift
+//  fartsound_app
+//
+//  Created by 김용훈 on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct fartsound_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fartsound_appTests/fartsound_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  fartsound_appTests.swift
+//  fartsound_appTests
+//
+//  Created by 김용훈 on 10/14/25.
+//
+
+import Testing
+@testable import fartsound_app
+
+struct fartsound_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
